f4613fa44c8ed8613a7cfb3704a2c695e76e6116
Eclipse <Eclips77@users.noreply.github.com>
2025-05-27 09:44:11 +0300

    Add project files.

---
+++ Commandos/Commando.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class Commando
+    {
+        string Name;
+        string CodeName;
+        List<string> Tools;
+        string Status;
+
+        public Commando(string name, string codeName,)
+
+    }
+}
